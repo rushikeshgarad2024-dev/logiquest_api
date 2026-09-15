31dd87e879493bca048d9e947eafdfc2f87ad1a1
Rushikesh Garad <rushikeshgarad2024@gmail.com>
2026-09-15 20:22:58 +0530

    feat(puzzles): register PuzzleVersion in PuzzlesModule

--- src/puzzles/puzzles.module.ts
+++ src/puzzles/puzzles.module.ts
@@ -3,12 +3,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { PuzzlesService } from './puzzles.service';
 import { PuzzlesController } from './puzzles.controller';
 import { Puzzle } from './entities/puzzle.entity';
+import { PuzzleVersion } from './entities/puzzle-version.entity';
 import { PuzzleTranslation } from './entities/puzzle-translation.entity';
 import { Category } from '../categories/entities/category.entity';
 import { Tag } from '../tags/entities/tag.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Puzzle, PuzzleTranslation, Category, Tag])],
+  imports: [TypeOrmModule.forFeature([Puzzle, PuzzleVersion, PuzzleTranslation, Category, Tag])],
   controllers: [PuzzlesController],
   providers: [PuzzlesService],
   exports: [PuzzlesService, TypeOrmModule],
